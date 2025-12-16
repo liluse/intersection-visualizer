f8011019eb1c76d905fa69a42eb7d2db69f1090b
Natalia <pietrzaknatalia2598@gmail.com>
2025-12-16 20:28:08 +0100

    initial config

---
+++ apps/visualizer/src/react/AppContainer.tsx
@@ -0,0 +1,13 @@
+// src/components/react/AppContainer.jsx
+import React, { useState } from 'react';
+
+const AppContainer = () => {
+
+  return (
+    <div className='m-5 text-center'>
+      React
+    </div>
+  );
+}
+
+export default AppContainer;
